c13e07a083369f5b38b910a7ea0eafaaa4404518
jiaxin <edison7500@gmail.com>
2020-01-04 21:35:50 +0800

    update

---
+++ src/components/Breadcrumb/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul></ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'd-breadcrumb',
+  props: {
+    items: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
